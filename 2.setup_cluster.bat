de9b0d91a6f345492abd848a470c687109529356
niall hennessy <niall hennessy>
2017-11-14 15:42:22 +0000

    files

---
+++ 2.setup_cluster.bat
@@ -0,0 +1,12 @@
+REM #
+REM #---------------------------------------------------#
+REM #						        #
+REM # 4. Set chunksize, Create Replica Sets and Shards  #
+REM #						        #
+REM #---------------------------------------------------#
+REM #	
+C:\"Program Files\MongoDB\Server\3.0\bin\mongo.exe" --shell 2.setup_cluster.js
+REM #
+
+
+
