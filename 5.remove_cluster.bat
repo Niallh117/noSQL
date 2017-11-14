b20cf56013d83dcf4d9a0e3fc7ab45fa2b59e07d
Niallh117 <33288609+Niallh117@users.noreply.github.com>
2017-11-14 15:54:56 +0000

    Add files via upload

---
+++ 5.remove_cluster.bat
@@ -0,0 +1,27 @@
+REM #
+REM #---------------------------------------------------#
+REM #						        #
+REM # 7. Remove Cluster				        #
+REM #						        #
+REM #---------------------------------------------------#
+REM #	
+RMDIR /S /Q C:\Users\niall\Desktop\Final year\nosql\cfg0
+RMDIR /S /Q C:\Users\niall\Desktop\Final year\nosql\cfg1
+RMDIR /S /Q C:\Users\niall\Desktop\Final year\nosql\cfg2
+REM # 
+RMDIR /S /Q C:\Users\niall\Desktop\Final year\nosql\dublin0
+RMDIR /S /Q C:\Users\niall\Desktop\Final year\nosql\dublin1
+RMDIR /S /Q C:\Users\niall\Desktop\Final year\nosql\dublin2
+REM # 
+RMDIR /S /Q C:\Users\niall\Desktop\Final year\nosql\cork0
+RMDIR /S /Q C:\Users\niall\Desktop\Final year\nosql\cork1
+RMDIR /S /Q C:\Users\niall\Desktop\Final year\nosql\cork2
+REM # 
+RMDIR /S /Q C:\Users\niall\Desktop\Final year\nosql\limerick0
+RMDIR /S /Q C:\Users\niall\Desktop\Final year\nosql\limerick1
+RMDIR /S /Q C:\Users\niall\Desktop\Final year\nosql\limerick2
+REM # 
+RMDIR /S /Q C:\Users\niall\Desktop\Final year\nosql\galway0
+RMDIR /S /Q C:\Users\niall\Desktop\Final year\nosql\galway1
+RMDIR /S /Q C:\Users\niall\Desktop\Final year\nosql\galway2
+REM # 
